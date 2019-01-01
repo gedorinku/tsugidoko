2b703b3b638dac4824faa19f3fabd6e088dbfc2f
hunachi <hanahanahunachi@gmail.com>
2019-01-01 18:02:14 +0900

    create login.

--- app/src/main/kotlin/io/github/hunachi/tsugidoko/appModule.kt
+++ app/src/main/kotlin/io/github/hunachi/tsugidoko/appModule.kt
@@ -4,15 +4,19 @@ import io.github.hunachi.tsugidoko.infra.SessionServiceClient
 import io.github.hunachi.tsugidoko.infra.UserServiceClient
 import io.github.hunachi.tsugidoko.login.LoginViewModel
 import io.github.hunachi.tsugidoko.map.MapViewModel
+import io.github.hunachi.tsugidoko.util.setupSharedPreference
 import org.koin.androidx.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
 
 val appModule = module {
+
+    factory { setupSharedPreference(get()) }
+
     factory { SessionServiceClient(get()) }
 
     factory { UserServiceClient() }
 
     viewModel { MapViewModel() }
 
-    viewModel { LoginViewModel(get()) }
+    viewModel { LoginViewModel(get(), get()) }
 }
--- app/src/main/kotlin/io/github/hunachi/tsugidoko/login/LoginViewModel.kt
+++ app/src/main/kotlin/io/github/hunachi/tsugidoko/login/LoginViewModel.kt
@@ -1,15 +1,18 @@
 package io.github.hunachi.tsugidoko.login
 
+import android.content.SharedPreferences
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import gedorinku.tsugidoko_server.Users
 import io.github.hunachi.tsugidoko.infra.SessionServiceClient
-import io.github.hunachi.tsugidoko.infra.UserServiceClient
 import io.github.hunachi.tsugidoko.util.NetworkState
+import io.github.hunachi.tsugidoko.util.session
 import kotlinx.coroutines.*
 
-class LoginViewModel(private val client: SessionServiceClient) : ViewModel() {
+class LoginViewModel(
+        private val client: SessionServiceClient,
+        private val preference: SharedPreferences
+) : ViewModel() {
 
     private val job = SupervisorJob()
     private val scope = Dispatchers.IO + job
@@ -17,11 +20,19 @@ class LoginViewModel(private val client: SessionServiceClient) : ViewModel() {
     private val _submitStatus = MutableLiveData<NetworkState<String>>()
     val submitStatus: LiveData<NetworkState<String>> = _submitStatus
 
-    fun submit(userName: String, password: String) {
+    fun submit(userName: String, password: String, isRegister: Boolean) {
         CoroutineScope(scope).launch {
             try {
-                val hoge = async { client.firstCreateSession(userName, password) }
-                _submitStatus.postValue(NetworkState.Success(hoge.await().sessionId))
+                val session = async {
+                    if (isRegister) {
+                        client.firstCreateSession(userName, password)
+                    } else {
+                        client.createSession(userName, password)
+                    }
+                }
+                val sessionId = session.await().sessionId
+                preference.session(sessionId)
+                _submitStatus.postValue(NetworkState.Success(sessionId))
             } catch (e: Exception) {
                 e.printStackTrace()
             }
