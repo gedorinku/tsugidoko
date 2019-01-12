a459e7787531f654ae599d12f87dcccd431d19b8
Ryota Egusa <gedorinku@yahoo.co.jp>
2019-01-12 17:04:45 +0900

    Fix model

--- app/src/main/kotlin/io/github/hunachi/tsugidoko/model/DetailMap.kt
+++ app/src/main/kotlin/io/github/hunachi/tsugidoko/model/DetailMap.kt
@@ -4,7 +4,7 @@ import java.io.Serializable
 
 data class DetailMap(
         val id: Int,
+        val buildingId: Int,
         val name: String,
-        val imageUrl: String,
         val rooms: List<FloorRooms>
 ) : Serializable
--- app/src/main/kotlin/io/github/hunachi/tsugidoko/model/Dummy.kt
+++ app/src/main/kotlin/io/github/hunachi/tsugidoko/model/Dummy.kt
@@ -5,14 +5,14 @@ import java.util.*
 
 val dummyDetailMap = DetailMap(
         0,
+        0,
         "sample",
-        "https://avatars0.githubusercontent.com/u/16878520?s=400&u=ca6ec3a48a4dd795a8783c432d4f2ee02fa531d6&v=4",
         listOf(dummyFloorRooms(0), dummyFloorRooms(1), dummyFloorRooms(2), dummyFloorRooms(3))
 )
 
 fun dummyFloorRooms(num: Int) = FloorRooms(
         num,
-        num,
+        "https://gedorinku.github.io/tsugidoko-pic/1/1.png",
         listOf(dummyClassRoom(0), dummyClassRoom(1))
 )
 
