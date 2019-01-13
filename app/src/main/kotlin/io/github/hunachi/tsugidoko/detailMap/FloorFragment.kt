db777f404bfcb922b4c7a02b2befb31cefd99e3d
Ryota Egusa <gedorinku@yahoo.co.jp>
2019-01-13 23:21:43 +0900

    Refactor

--- app/src/main/kotlin/io/github/hunachi/tsugidoko/detailMap/FloorFragment.kt
+++ app/src/main/kotlin/io/github/hunachi/tsugidoko/detailMap/FloorFragment.kt
@@ -1,20 +1,16 @@
 package io.github.hunachi.tsugidoko.detailMap
 
-import android.content.Context
 import android.graphics.Matrix
 import android.os.Bundle
-import android.telecom.Call
-import android.util.Log
-import android.view.Gravity
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ImageView
-import androidx.core.graphics.translationMatrix
 import androidx.core.os.bundleOf
-import androidx.core.view.*
+import androidx.core.view.marginLeft
+import androidx.core.view.marginTop
+import androidx.fragment.app.Fragment
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import io.github.hunachi.tsugidoko.R
@@ -22,7 +18,6 @@ import io.github.hunachi.tsugidoko.model.ClassRoom
 import io.github.hunachi.tsugidoko.model.FloorRooms
 import io.github.hunachi.tsugidoko.util.inflate
 import kotlinx.android.synthetic.main.fragment_floor.view.*
-import java.lang.Exception
 
 
 class FloorFragment : Fragment() {
@@ -73,26 +68,18 @@ class FloorFragment : Fragment() {
 
                 // detail mapの画像内でのマーカーの位置[dp]
                 // ImageViewの中央によっているので、2 * mapTransX、2 * mapTransYをそれぞれ引いておく
-                val x = classRoom.detailPosition.x * (mapImageView.width - 2 * mapTransX)// / density
-                val y= classRoom.detailPosition.y * (mapImageView.height - 2 * mapTransY)// / density
+                val x = classRoom.detailPosition.x * (mapImageView.width - 2 * mapTransX)
+                val y = classRoom.detailPosition.y * (mapImageView.height - 2 * mapTransY)
 
                 val transX = mapImageView.marginLeft + mapTransX.toInt() + x.toInt()
                 val transY = mapImageView.marginTop + mapTransY.toInt() + y.toInt()
                 setMargins(transX, transY, 0, 0)
             }
         }
-        Log.d("map", mapImageView.matrix.toString() + "     " + mapImageView.imageMatrix.toString())
         Picasso.get()
                 .load(R.drawable.people)
                 .fit()
-                .into(imageView, object : Callback {
-                    override fun onSuccess() {
-                        Log.d("icon", imageView.matrix.toString() + "     " + imageView.imageMatrix.toString())
-                    }
-
-                    override fun onError(e: Exception?) {}
-
-                })
+                .into(imageView)
         frame.addView(imageView)
     }
 
